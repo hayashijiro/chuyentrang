a1cea43cf5eaa34a7085e1730eab744f0dea5a25
Hayashi Jiro <89639320+hayashijiro@users.noreply.github.com>
2022-12-16 23:17:16 +0700

    Update script.js
    Signed-off-by: Hayashi Jiro <89639320+hayashijiro@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-function anh() {
-    window.open="https://hayashijiro.github.io/dichanh/";
-}
-function mic() {
-    window.open="https://hayashijiro.github.io/dichamthanh/";
-}
+
+let mic = document.getElementById('mybtn2');
+button2.addEventListener('click', () => {
+  let windowFeatures = "left=100,top=100,width=320,height=320";
+  window.open("https://hayashijiro.github.io/dichamthanh/", "amthanh",  windowFeatures);
+
+});
+let anh = document.getElementById('mybtn2');
+button2.addEventListener('click', () => {
+  let windowFeatures = "left=100,top=100,width=320,height=320";
+  window.open("https://hayashijiro.github.io/dichanh/", "anh",  windowFeatures);
+
+});
+
