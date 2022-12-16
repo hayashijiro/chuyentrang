1a9d65c3029069d9220faf32caa0e70da21b80c9
Hayashi Jiro <89639320+hayashijiro@users.noreply.github.com>
2022-12-16 23:25:59 +0700

    Add files via upload
    Signed-off-by: Hayashi Jiro <89639320+hayashijiro@users.noreply.github.com>

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let mic = document.getElementById('mybtn2');
+mic.addEventListener('click', () => {
+  let windowFeatures = "left=100,top=100,width=320,height=320";
+  window.open("https://hayashijiro.github.io/dichamthanh/",  windowFeatures);
+
+});
+let anh = document.getElementById('mybtn1');
+anh.addEventListener('click', () => {
+  let windowFeatures = "left=100,top=100,width=320,height=320";
+  window.open("https://hayashijiro.github.io/dichanh/",  windowFeatures);
+
+});
